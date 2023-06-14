179b0db7853c5c8bbf731b92c925d3324523080a
Jaci Cotaquispe <jc104@wellesley.edu>
2023-06-14 17:26:46 -0400

    first changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import cody from "/Users/jacicotaquispe/to-do-list_week1-jaci-cotaquispe/src//assets/cody.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+            <div className="split left">
+              <div className="centered">
+                <img 
+                  className="profile_image"
+                  src = "cody.jpg"
+                  alt ="Profile Pic"
+                  ></img>
+              </div>
+            </div>
+            <div className="split right">
+              <div className="centered">
+                <div className="name_title"> Jaci Cotaquispe </div>
+                <div className="brief_description">
+                  // Details about you goes here
+                </div>
+              </div>
+            </div>
+  </div> 
       </div>
     )
   }
